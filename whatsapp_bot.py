43e611b7e4de478342820a472200673c69acf83b
Braulio A. Sassi <braulio.sassi@gmail.com>
2020-08-19 22:00:05 -0300

    inserido import selenium

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,35 +1,36 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+
 
 class WhatsappBot:
-   	def __init__(self):
-	   	# Parte 1 - A mensagem que você quer enviar
-    	self.mensagem = "Bom dia pessoal, veja o video que acabou de sair https://www.youtube.com"
-    	# Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
-    	self.grupos_ou_pessoas = ["TI-UFRJ"]
-    	options = webdriver.ChromeOptions()
-    	options.add_argument('lang=pt-br')
-    	self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', chrome_options=options)
-	
-	def EnviarMensagens(self):
+    def __init__(self):
+        # Parte 1 - A mensagem que você quer enviar
+        self.mensagem = "Bom dia pessoal, veja o video que acabou de sair https://www.youtube.com"
+        # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
+        self.grupos_ou_pessoas = ["TI-UFRJ"]
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(
+            executable_path=r'./chromedriver.exe', chrome_options=options)
+    def EnviarMensagens(self):
 	    self.driver.get('https://web.whatsapp.com')
 	    time.sleep(30)
-		for grupo_ou_pessoa in self.grupos_ou_pessoas:
-	    campo_grupo = self.driver.find_element_by_xpath(
-		    f"//span[@title='{grupo_ou_pessoa}']")
-	    	time.sleep(3)
-	    	campo_grupo.click()
-	    	chat_box = self.driver.find_element_by_class_name('_13mgZ')
-	    	time.sleep(3)
-	    	chat_box.click()
-	    	chat_box.send_keys(self.mensagem)
+	    for grupo_ou_pessoa in self.grupos_ou_pessoas:
+	        campo_grupo = self.driver.find_element_by_xpath(
+	            f"//span[@title='{grupo_ou_pessoa}']")
+	        time.sleep(3)
+	        campo_grupo.click()
+	        chat_box = self.driver.find_element_by_class_name('_13mgZ')
+	        time.sleep(3)
+	        chat_box.click()
+	        chat_box.send_keys(self.mensagem)
 	        botao_enviar = self.driver.find_element_by_xpath(
-	    	"//span[@data-icon='send']")
-	    	time.sleep(3)
-	    	botao_enviar.click()
-	    	time.sleep(5)
+	            "//span[@data-icon='send']")
+	        time.sleep(3)
+	        botao_enviar.click()
+	        time.sleep(5)
 
 
 bot = WhatsappBot()
-bot.EnviarMensagens()		
+bot.EnviarMensagens()
